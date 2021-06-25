64fcb141ae0c4cd037182063d5bc6d3ffd33043e
sipp11 <sipp11@zzyzx.co>
2021-06-25 22:11:27 +0700

    Simple SSV picker

--- components/JobOverly.js
+++ components/JobOverly.js
@@ -4,20 +4,56 @@ import {activateKeepAwake, deactivateKeepAwake} from 'expo-keep-awake';
 import gql from 'graphql-tag';
 import React, {useEffect, useState} from 'react';
 import {useTranslation} from 'react-i18next';
-import {StyleSheet, Text, View} from 'react-native';
+import {Modal, Image, StyleSheet, Text, View} from 'react-native';
 import {Icon} from 'react-native-elements';
 import {TouchableOpacity} from 'react-native-gesture-handler';
+import {SvgXml} from 'react-native-svg';
 import {hhmmDuration} from '../libs/day';
 import GeoLocationProvider from './GeoLocationProvider';
+import {modelStyles} from './Modal';
+import SsvModel1 from './ssv-model-1.svg';
+import SsvModel2 from './ssv-model-2.svg';
+import SsvModel3 from './ssv-model-3.svg';
 
 const GREEN = 'rgba(68, 252, 148, 0.3)';
 const YELLOW = 'rgba(252, 186, 3, 0.5)';
 const RED = 'rgba(249, 88, 67, 0.5)';
 
+const xml = `
+  <svg width="32" height="32" viewBox="0 0 32 32">
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      fill="url(#gradient)"
+      d="M4 0C1.79086 0 0 1.79086 0 4V28C0 30.2091 1.79086 32 4 32H28C30.2091 32 32 30.2091 32 28V4C32 1.79086 30.2091 0 28 0H4ZM17 6C17 5.44772 17.4477 5 18 5H20C20.5523 5 21 5.44772 21 6V25C21 25.5523 20.5523 26 20 26H18C17.4477 26 17 25.5523 17 25V6ZM12 11C11.4477 11 11 11.4477 11 12V25C11 25.5523 11.4477 26 12 26H14C14.5523 26 15 25.5523 15 25V12C15 11.4477 14.5523 11 14 11H12ZM6 18C5.44772 18 5 18.4477 5 19V25C5 25.5523 5.44772 26 6 26H8C8.55228 26 9 25.5523 9 25V19C9 18.4477 8.55228 18 8 18H6ZM24 14C23.4477 14 23 14.4477 23 15V25C23 25.5523 23.4477 26 24 26H26C26.5523 26 27 25.5523 27 25V15C27 14.4477 26.5523 14 26 14H24Z"
+    />
+    <defs>
+      <linearGradient
+        id="gradient"
+        x1="0"
+        y1="0"
+        x2="8.46631"
+        y2="37.3364"
+        gradient-units="userSpaceOnUse">
+        <stop offset="0" stop-color="#FEA267" />
+        <stop offset="1" stop-color="#E75A4C" />
+      </linearGradient>
+    </defs>
+  </svg>
+`;
+
+function getVehicleID(data) {
+  if (!data) return '';
+  if (!data.working_shift) return '';
+  if (data.working_shift.length === 0) return '';
+  return data.working_shift[0].vehicle_id;
+}
+
 export default function JobOverlay({isWorking, setWorking, user}) {
   const {t} = useTranslation();
   const [geo, setGeo] = useState(null);
   const [lastGeoTimestamp, setLastGeoTimestamp] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
   const [shiftID, setShiftID] = useState(-1);
   const {data, loading} = useSubscription(ACTIVE_WORKING_SHIFT, {
     variables: {userId: user.id},
@@ -60,14 +96,17 @@ export default function JobOverlay({isWorking, setWorking, user}) {
   }, [isWorking]);
 
   useEffect(async () => {
+    if (!geo) return;
     if (geo.error) return;
     if (!geo.lastPosition) return;
     const {coords, timestamp} = geo.lastPosition;
+    if (!coords) return;
     // if (timestamp > lastGeoTimestamp) {
     // TODO: update last coords to working-shift
     setLastGeoTimestamp(timestamp);
-    console.log('new geo: ', shiftID, timestamp)
+    console.log('new geo: ', shiftID, timestamp);
     if (shiftID && !updateLoading) {
+      console.log('coords: ', coords);
       const variables = {
         ID: shiftID,
         point: {
@@ -84,6 +123,66 @@ export default function JobOverlay({isWorking, setWorking, user}) {
   return (
     <View style={styles.container}>
       <GeoLocationProvider isActive={isWorking} handleGeoInfo={setGeo} />
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          //Alert.alert('Modal has been closed.');
+        }}>
+        <View style={modelStyles.centeredView}>
+          <View style={modelStyles.modalView}>
+            <View style={modelStyles.closeButton}>
+              <TouchableOpacity
+                onPress={() => {
+                  setModalVisible(false);
+                }}>
+                <Icon
+                  raised
+                  name="ios-close"
+                  type="ionicon"
+                  containerStyle={{backgroundColor: '#000000'}}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.colFlex}>
+              <TouchableOpacity
+                onPress={async () => {
+                  const variables = {vehicleID: 1};
+                  const res = await startWorking({variables});
+                  const {id} = res.data.insert_working_shift_one;
+                  setShiftID(id);
+                  setWorking(true);
+                  setModalVisible(!modalVisible);
+                }}>
+                <SvgXml xml={SsvModel1} width="150" height="150" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={async () => {
+                  const variables = {vehicleID: 2};
+                  const res = await startWorking({variables});
+                  const {id} = res.data.insert_working_shift_one;
+                  setShiftID(id);
+                  setWorking(true);
+                  setModalVisible(!modalVisible);
+                }}>
+                <SvgXml xml={SsvModel2} width="150" height="150" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={async () => {
+                  const variables = {vehicleID: 3};
+                  const res = await startWorking({variables});
+                  const {id} = res.data.insert_working_shift_one;
+                  setShiftID(id);
+                  setWorking(true);
+                  setModalVisible(!modalVisible);
+                }}>
+                <SvgXml xml={SsvModel3} width="150" height="150" />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
       {!isWorking && (
         <View
           style={[
@@ -102,12 +201,13 @@ export default function JobOverlay({isWorking, setWorking, user}) {
                 justifyContent: 'center',
                 alignItems: 'center',
               }}
-              onPress={async () => {
-                const variables = {vehicleID: 1};
-                const res = await startWorking({variables});
-                const {id} = res.data.insert_working_shift_one;
-                setShiftID(id);
-                setWorking(true);
+              onPress={() => {
+                // const variables = {vehicleID: 1};
+                // const res = await startWorking({variables});
+                // const {id} = res.data.insert_working_shift_one;
+                // setShiftID(id);
+                // setWorking(true);
+                setModalVisible(!modalVisible);
               }}>
               <Text>{t('job.WorkingInitButtonLabel')}</Text>
             </TouchableOpacity>
@@ -116,7 +216,9 @@ export default function JobOverlay({isWorking, setWorking, user}) {
       )}
       {isWorking && shiftID > -1 && (
         <>
-          <Text style={styles.timer}>{shiftDuration}</Text>
+          <Text style={styles.timer}>
+            [#{getVehicleID(data)}] {shiftDuration}
+          </Text>
           <Icon
             raised
             name="pause-outline"
